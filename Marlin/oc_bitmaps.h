d607bf852b333d6376cc20242fdd7068746dac00
JS <dooly9822@gmail.com>
2019-12-07 04:27:06 +0900

    oc bitmap, icons, lcd 파일 추가

---
+++ Marlin/oc_bitmaps.h
@@ -0,0 +1,381 @@
+// letters ////////////////////////////////////////////////////////////////////
+static const uint8_t bitmap_letter_0[] PROGMEM = {
+	#include "bitmaps/letter_0.png.inc"
+};
+
+static const uint8_t bitmap_letter_1[] PROGMEM = {
+	#include "bitmaps/letter_1.png.inc"
+};
+
+static const uint8_t bitmap_letter_2[] PROGMEM = {
+	#include "bitmaps/letter_2.png.inc"
+};
+
+static const uint8_t bitmap_letter_3[] PROGMEM = {
+	#include "bitmaps/letter_3.png.inc"
+};
+
+static const uint8_t bitmap_letter_4[] PROGMEM = {
+	#include "bitmaps/letter_4.png.inc"
+};
+
+static const uint8_t bitmap_letter_5[] PROGMEM = {
+	#include "bitmaps/letter_5.png.inc"
+};
+
+static const uint8_t bitmap_letter_6[] PROGMEM = {
+	#include "bitmaps/letter_6.png.inc"
+};
+
+static const uint8_t bitmap_letter_7[] PROGMEM = {
+	#include "bitmaps/letter_7.png.inc"
+};
+
+static const uint8_t bitmap_letter_8[] PROGMEM = {
+	#include "bitmaps/letter_8.png.inc"
+};
+
+static const uint8_t bitmap_letter_9[] PROGMEM = {
+	#include "bitmaps/letter_9.png.inc"
+};
+
+
+// A - Z
+static const uint8_t bitmap_letter_A[] PROGMEM = {
+	#include "bitmaps/letter_A.png.inc"
+};
+
+static const uint8_t bitmap_letter_B[] PROGMEM = {
+	#include "bitmaps/letter_B.png.inc"
+};
+
+static const uint8_t bitmap_letter_C[] PROGMEM = {
+	#include "bitmaps/letter_C.png.inc"
+};
+
+static const uint8_t bitmap_letter_D[] PROGMEM = { 
+	#include "bitmaps/letter_D.png.inc"
+};
+
+static const uint8_t bitmap_letter_E[] PROGMEM = { 
+	#include "bitmaps/letter_E.png.inc"
+};
+
+static const uint8_t bitmap_letter_F[] PROGMEM = { 
+	#include "bitmaps/letter_F.png.inc"
+};
+
+static const uint8_t bitmap_letter_G[] PROGMEM = { 
+	#include "bitmaps/letter_G.png.inc"
+};
+
+static const uint8_t bitmap_letter_H[] PROGMEM = { 
+	#include "bitmaps/letter_H.png.inc"
+};
+
+static const uint8_t bitmap_letter_I[] PROGMEM = { 
+	#include "bitmaps/letter_I.png.inc"
+};
+
+static const uint8_t bitmap_letter_J[] PROGMEM = { 
+	#include "bitmaps/letter_J.png.inc"
+};
+
+static const uint8_t bitmap_letter_K[] PROGMEM = { 
+	#include "bitmaps/letter_K.png.inc"
+};
+
+static const uint8_t bitmap_letter_L[] PROGMEM = { 
+	#include "bitmaps/letter_L.png.inc"
+};
+
+static const uint8_t bitmap_letter_M[] PROGMEM = { 
+	#include "bitmaps/letter_M.png.inc"
+};
+
+static const uint8_t bitmap_letter_N[] PROGMEM = { 
+	#include "bitmaps/letter_N.png.inc"
+};
+
+static const uint8_t bitmap_letter_O[] PROGMEM = { 
+	#include "bitmaps/letter_O.png.inc"
+};
+
+static const uint8_t bitmap_letter_P[] PROGMEM = { 
+	#include "bitmaps/letter_P.png.inc"
+};
+
+static const uint8_t bitmap_letter_Q[] PROGMEM = { 
+	#include "bitmaps/letter_Q.png.inc"
+};
+
+static const uint8_t bitmap_letter_R[] PROGMEM = { 
+	#include "bitmaps/letter_R.png.inc"
+};
+
+static const uint8_t bitmap_letter_S[] PROGMEM = { 
+	#include "bitmaps/letter_S.png.inc"
+};
+
+static const uint8_t bitmap_letter_T[] PROGMEM = { 
+	#include "bitmaps/letter_T.png.inc"
+};
+
+static const uint8_t bitmap_letter_U[] PROGMEM = { 
+	#include "bitmaps/letter_U.png.inc"
+};
+
+static const uint8_t bitmap_letter_V[] PROGMEM = { 
+	#include "bitmaps/letter_V.png.inc"
+};
+
+static const uint8_t bitmap_letter_W[] PROGMEM = { 
+	#include "bitmaps/letter_W.png.inc"
+};
+
+static const uint8_t bitmap_letter_X[] PROGMEM = { 
+	#include "bitmaps/letter_X.png.inc"
+};
+
+static const uint8_t bitmap_letter_Y[] PROGMEM = { 
+	#include "bitmaps/letter_Y.png.inc"
+};
+
+static const uint8_t bitmap_letter_Z[] PROGMEM = { 
+	#include "bitmaps/letter_Z.png.inc"
+};
+
+// a - z
+static const uint8_t bitmap_letter_sa[] PROGMEM = { 
+	#include "bitmaps/letter_sa.png.inc"
+};
+
+static const uint8_t bitmap_letter_sb[] PROGMEM = { 
+	#include "bitmaps/letter_sb.png.inc"
+};
+
+static const uint8_t bitmap_letter_sc[] PROGMEM = { 
+	#include "bitmaps/letter_sc.png.inc"
+};
+
+static const uint8_t bitmap_letter_sd[] PROGMEM = { 
+	#include "bitmaps/letter_sd.png.inc"
+};
+
+static const uint8_t bitmap_letter_se[] PROGMEM = { 
+	#include "bitmaps/letter_se.png.inc"
+};
+
+static const uint8_t bitmap_letter_sf[] PROGMEM = { 
+	#include "bitmaps/letter_sf.png.inc"
+};
+
+static const uint8_t bitmap_letter_sg[] PROGMEM = { 
+	#include "bitmaps/letter_sg.png.inc"
+};
+
+static const uint8_t bitmap_letter_sh[] PROGMEM = { 
+	#include "bitmaps/letter_sh.png.inc"
+};
+
+static const uint8_t bitmap_letter_si[] PROGMEM = { 
+	#include "bitmaps/letter_si.png.inc"
+};
+
+static const uint8_t bitmap_letter_sj[] PROGMEM = { 
+	#include "bitmaps/letter_sj.png.inc"
+};
+
+static const uint8_t bitmap_letter_sk[] PROGMEM = { 
+	#include "bitmaps/letter_sk.png.inc"
+};
+
+static const uint8_t bitmap_letter_sl[] PROGMEM = { 
+	#include "bitmaps/letter_sl.png.inc"
+};
+
+static const uint8_t bitmap_letter_sm[] PROGMEM = { 
+	#include "bitmaps/letter_sm.png.inc"
+};
+
+static const uint8_t bitmap_letter_sn[] PROGMEM = { 
+	#include "bitmaps/letter_sn.png.inc"
+};
+
+static const uint8_t bitmap_letter_so[] PROGMEM = { 
+	#include "bitmaps/letter_so.png.inc"
+};
+
+static const uint8_t bitmap_letter_sp[] PROGMEM = { 
+	#include "bitmaps/letter_sp.png.inc"
+};
+
+static const uint8_t bitmap_letter_sq[] PROGMEM = { 
+	#include "bitmaps/letter_sq.png.inc"
+};
+
+static const uint8_t bitmap_letter_sr[] PROGMEM = { 
+	#include "bitmaps/letter_sr.png.inc"
+};
+
+static const uint8_t bitmap_letter_ss[] PROGMEM = { 
+	#include "bitmaps/letter_ss.png.inc"
+};
+
+static const uint8_t bitmap_letter_st[] PROGMEM = { 
+	#include "bitmaps/letter_st.png.inc"
+};
+
+static const uint8_t bitmap_letter_su[] PROGMEM = { 
+	#include "bitmaps/letter_su.png.inc"
+};
+
+static const uint8_t bitmap_letter_sv[] PROGMEM = { 
+	#include "bitmaps/letter_sv.png.inc"
+};
+
+static const uint8_t bitmap_letter_sw[] PROGMEM = { 
+	#include "bitmaps/letter_sw.png.inc"
+};
+
+static const uint8_t bitmap_letter_sx[] PROGMEM = { 
+	#include "bitmaps/letter_sx.png.inc"
+};
+
+static const uint8_t bitmap_letter_sy[] PROGMEM = { 
+	#include "bitmaps/letter_sy.png.inc"
+};
+
+static const uint8_t bitmap_letter_sz[] PROGMEM = { 
+	#include "bitmaps/letter_sz.png.inc"
+};
+
+
+// : , . do
+static const uint8_t bitmap_letter_colon[] PROGMEM = { 
+	#include "bitmaps/letter_colon.png.inc"
+};
+static const uint8_t bitmap_letter_comma[] PROGMEM = { 
+	#include "bitmaps/letter_comma.png.inc"
+};
+static const uint8_t bitmap_letter_period[] PROGMEM = { 
+	#include "bitmaps/letter_period.png.inc"
+};
+static const uint8_t bitmap_letter_do[] PROGMEM = { 
+	#include "bitmaps/letter_do.png.inc"
+};
+static const uint8_t bitmap_letter_slash[] PROGMEM = { 
+	#include "bitmaps/letter_slash.png.inc"
+};
+static const uint8_t bitmap_letter_sqbl[] PROGMEM = { 
+	#include "bitmaps/letter_sqbl.png.inc"
+};
+static const uint8_t bitmap_letter_sqbr[] PROGMEM = { 
+	#include "bitmaps/letter_sqbr.png.inc"
+};
+static const uint8_t bitmap_letter_minus[] PROGMEM = { 
+	#include "bitmaps/letter_minus.png.inc"
+};
+static const uint8_t bitmap_letter_spl[] PROGMEM = { 
+	#include "bitmaps/letter_spl.png.inc"
+};
+static const uint8_t bitmap_letter_spr[] PROGMEM = { 
+	#include "bitmaps/letter_spr.png.inc"
+};
+static const uint8_t bitmap_letter_percent[] PROGMEM = { 
+	#include "bitmaps/letter_percent.png.inc"
+};
+
+#if 0
+// logo //////////////////////////////////////////////////////////////////////
+/*
+static const uint8_t bitmap_logo0[] PROGMEM = {
+	#include "icons/oc_logo0.png.inc"
+};
+static const uint8_t bitmap_logo1[] PROGMEM = {
+	#include "icons/oc_logo1.png.inc"
+};
+*/
+static const uint8_t bitmap_copyrights[] PROGMEM = {
+	#include "icons/oc_copyrights.png.inc"
+};
+
+// icons //////////////////////////////////////////////////////////////////////
+static const uint8_t icon20x20_time[] PROGMEM = { 
+	#include "icons/icon20x20_time.png.inc"
+};
+static const uint8_t icon20x20_weight[] PROGMEM = { 
+	#include "icons/icon20x20_weight.png.inc"
+};
+static const uint8_t icon32x33_nozzle_temp[] PROGMEM = { 
+	#include "icons/icon32x33_nozzle_temp.png.inc"
+};
+
+
+static const uint8_t icon50x50_down[] PROGMEM = { 
+	#include "icons/icon50x50_down.png.inc"
+};
+static const uint8_t icon50x50_end[] PROGMEM = { 
+	#include "icons/icon50x50_end.png.inc"
+};
+static const uint8_t icon50x50_ffwd[] PROGMEM = { 
+	#include "icons/icon50x50_ffwd.png.inc"
+};
+static const uint8_t icon50x50_g29[] PROGMEM = { 
+	#include "icons/icon50x50_g29.png.inc"
+};
+static const uint8_t icon50x50_home[] PROGMEM = { 
+	#include "icons/icon50x50_home.png.inc"
+};
+static const uint8_t icon50x50_left[] PROGMEM = { 
+	#include "icons/icon50x50_left.png.inc"
+};
+static const uint8_t icon50x50_next[] PROGMEM = { 
+	#include "icons/icon50x50_next.png.inc"
+};
+static const uint8_t icon50x50_prev[] PROGMEM = { 
+	#include "icons/icon50x50_prev.png.inc"
+};
+static const uint8_t icon50x50_rewind[] PROGMEM = { 
+	#include "icons/icon50x50_rewind.png.inc"
+};
+static const uint8_t icon50x50_right[] PROGMEM = { 
+	#include "icons/icon50x50_right.png.inc"
+};
+static const uint8_t icon50x50_up[] PROGMEM = { 
+	#include "icons/icon50x50_up.png.inc"
+};
+
+
+static const uint8_t icon60x80_back[] PROGMEM = { 
+	#include "icons/icon60x80_back.png.inc"
+};
+static const uint8_t icon60x80_continue[] PROGMEM = { 
+	#include "icons/icon60x80_continue.png.inc"
+};
+static const uint8_t icon60x80_cooldown[] PROGMEM = { 
+	#include "icons/icon60x80_cooldown.png.inc"
+};
+static const uint8_t icon60x80_jog[] PROGMEM = { 
+	#include "icons/icon60x80_jog.png.inc"
+};
+static const uint8_t icon60x80_pause[] PROGMEM = { 
+	#include "icons/icon60x80_pause.png.inc"
+};
+static const uint8_t icon60x80_preheat[] PROGMEM = { 
+	#include "icons/icon60x80_preheat.png.inc"
+};
+static const uint8_t icon60x80_prepare[] PROGMEM = { 
+	#include "icons/icon60x80_prepare.png.inc"
+};
+static const uint8_t icon60x80_print[] PROGMEM = { 
+	#include "icons/icon60x80_print.png.inc"
+};
+static const uint8_t icon60x80_settings[] PROGMEM = { 
+	#include "icons/icon60x80_settings.png.inc"
+};
+static const uint8_t icon60x80_stop[] PROGMEM = { 
+	#include "icons/icon60x80_stop.png.inc"
+};
+
+#endif
---
+++ Marlin/oc_icons.h
@@ -0,0 +1,145 @@
+
+// logo //////////////////////////////////////////////////////////////////////
+/*
+static const uint8_t bitmap_logo0[] PROGMEM = {
+	#include "icons/oc_logo0.png.inc"
+};
+static const uint8_t bitmap_logo1[] PROGMEM = {
+	#include "icons/oc_logo1.png.inc"
+};
+*/
+static const uint8_t bitmap_copyrights[] PROGMEM = {
+	#include "icons/oc_copyrights.png.inc"
+};
+
+// icons //////////////////////////////////////////////////////////////////////
+static const uint8_t icon20x20_time[] PROGMEM = { 
+	#include "icons/icon20x20_time.png.inc"
+};
+static const uint8_t icon20x20_weight[] PROGMEM = { 
+	#include "icons/icon20x20_weight.png.inc"
+};
+static const uint8_t icon32x32_nozzle_temp[] PROGMEM = { 
+	#include "icons/icon32x32_nozzle_temp.png.inc"
+};
+static const uint8_t icon32x32_clock[] PROGMEM = { 
+	#include "icons/icon32x32_clock.png.inc"
+};
+
+
+/*
+static const uint8_t icon50x50_end[] PROGMEM = { 
+	#include "icons/icon50x50_end.png.inc"
+};
+static const uint8_t icon50x50_ffwd[] PROGMEM = { 
+	#include "icons/icon50x50_ffwd.png.inc"
+};
+static const uint8_t icon50x50_next[] PROGMEM = { 
+	#include "icons/icon50x50_next.png.inc"
+};
+static const uint8_t icon50x50_prev[] PROGMEM = { 
+	#include "icons/icon50x50_prev.png.inc"
+};
+static const uint8_t icon50x50_rewind[] PROGMEM = { 
+	#include "icons/icon50x50_rewind.png.inc"
+};
+*/
+static const uint8_t icon50x50_down[] PROGMEM = { 
+	#include "icons/icon50x50_down.png.inc"
+};
+static const uint8_t icon50x50_g29[] PROGMEM = { 
+	#include "icons/icon50x50_g29.png.inc"
+};
+static const uint8_t icon50x50_npdm[] PROGMEM = { 
+	#include "icons/icon50x50_npdm.png.inc"
+};
+static const uint8_t icon50x50_home[] PROGMEM = { 
+	#include "icons/icon50x50_home.png.inc"
+};
+static const uint8_t icon50x50_left[] PROGMEM = { 
+	#include "icons/icon50x50_left.png.inc"
+};
+static const uint8_t icon50x50_right[] PROGMEM = { 
+	#include "icons/icon50x50_right.png.inc"
+};
+static const uint8_t icon50x50_up[] PROGMEM = { 
+	#include "icons/icon50x50_up.png.inc"
+};
+static const uint8_t icon50x50_motoroff[] PROGMEM = { 
+	#include "icons/icon50x50_motoroff.png.inc"
+};
+/*
+static const uint8_t icon50x50_extract[] PROGMEM = { 
+	#include "icons/icon50x50_extract.png.inc"
+};
+static const uint8_t icon50x50_retract[] PROGMEM = { 
+	#include "icons/icon50x50_retract.png.inc"
+};
+*/
+
+static const uint8_t icon60x80_back[] PROGMEM = { 
+	#include "icons/icon60x80_back.png.inc"
+};
+static const uint8_t icon60x80_continue[] PROGMEM = { 
+	#include "icons/icon60x80_continue.png.inc"
+};
+#if 0
+static const uint8_t icon60x80_file[] PROGMEM = { 
+	#include "icons/icon60x80_file.png.inc"
+};
+#endif
+static const uint8_t icon60x80_cooldown[] PROGMEM = { 
+	#include "icons/icon60x80_cooldown.png.inc"
+};
+static const uint8_t icon60x80_jog[] PROGMEM = { 
+	#include "icons/icon60x80_jog.png.inc"
+};
+static const uint8_t icon60x80_pause[] PROGMEM = { 
+	#include "icons/icon60x80_pause.png.inc"
+};
+static const uint8_t icon60x80_preheat[] PROGMEM = { 
+	#include "icons/icon60x80_preheat.png.inc"
+};
+static const uint8_t icon60x80_prepare[] PROGMEM = { 
+	#include "icons/icon60x80_prepare.png.inc"
+};
+static const uint8_t icon60x80_print[] PROGMEM = { 
+	#include "icons/icon60x80_print.png.inc"
+};
+static const uint8_t icon60x80_settings[] PROGMEM = { 
+	#include "icons/icon60x80_settings.png.inc"
+};
+static const uint8_t icon60x80_stop[] PROGMEM = { 
+	#include "icons/icon60x80_stop.png.inc"
+};
+/*
+static const uint8_t icon60x80_extract[] PROGMEM = { 
+	#include "icons/icon60x80_extract.png.inc"
+};
+static const uint8_t icon60x80_retract[] PROGMEM = { 
+	#include "icons/icon60x80_retract.png.inc"
+};
+*/
+
+static const uint8_t text_fan_speed[] PROGMEM = { 
+	#include "icons/text_fan_speed.png.inc"
+};
+static const uint8_t text_nozzle_offset[] PROGMEM = { 
+	#include "icons/text_nozzle_offset.png.inc"
+};
+static const uint8_t text_nozzle_temp[] PROGMEM = { 
+	#include "icons/text_nozzle_temp.png.inc"
+};
+static const uint8_t text_speed[] PROGMEM = { 
+	#include "icons/text_speed.png.inc"
+};
+static const uint8_t text_print[] PROGMEM = { 
+	#include "icons/text_print.png.inc"
+};
+static const uint8_t text_settings[] PROGMEM = { 
+	#include "icons/text_settings.png.inc"
+};
+static const uint8_t text_to_mainmenu[] PROGMEM = { 
+	#include "icons/text_to_mainmenu.png.inc"
+};
+
