51e0551cf6de2099c101764390bd1693f3a67aa4
JONGSEO HAN <dooly9822@gmail.com>
2019-12-19 09:50:07 +0900

    파일 추가

---
+++ Marlin/Marlin/src/lcd/dogm/lcdprint_u8g.cpp
@@ -0,0 +1,93 @@
+/**
+ * @file    lcdprint_u8g.cpp
+ * @brief   LCD print api for u8glib
+ * @author  Yunhui Fu (yhfudev@gmail.com)
+ * @version 1.0
+ * @date    2016-08-19
+ * @copyright GPL/BSD
+ */
+
+#include "../../inc/MarlinConfigPre.h"
+
+#if HAS_GRAPHICAL_LCD
+
+#include "ultralcd_DOGM.h"
+
+#include "../ultralcd.h"
+#include "../../Marlin.h"
+
+#include "../fontutils.h"
+#include "../lcdprint.h"
+#include "oc_tft/oc_tft.h"
+
+int lcd_glyph_height() { return 240; }
+
+bool isOutofRange(int x, int y){
+	if( x > LCD_PIXEL_WIDTH || x < 0)
+		return true;
+	if( y > LCD_PIXEL_HEIGHT || y < 0)
+		return true;
+	return false;
+}
+
+void lcd_moveto(const lcd_uint_t col, const lcd_uint_t row) { 
+	setPrintPos(col, row); 
+}
+
+void lcd_put_int(const int i) { 
+	tft_put_int(i);
+}
+
+void readPGM(PGM_P str, char* pTargetStr) {
+  int idx=0;
+  while (const char c = pgm_read_byte(str++)) pTargetStr[idx++] = c;
+}
+
+// return < 0 on error
+// return the advanced pixels
+int lcd_put_wchar_max(wchar_t c, pixel_len_t max_length) {
+  uint16_t x = getPrintCol(), y = getPrintRow();
+  if( isOutofRange( x, y))
+  {
+	SERIAL_ECHOLN(x); SERIAL_ECHOLN(y);
+	return -1;
+  }
+
+  if (c < 256) {
+    return tft_printstr((char*)&c);
+  }
+  
+  uint16_t ret = tft_printf(x, y, 0, c);
+  setPrintPos(x + ret, y);
+  return ret;
+}
+
+int lcd_put_u8str_max(const char * utf8_str, pixel_len_t max_length) {
+  uint16_t x = getPrintCol(), y = getPrintRow();
+  if( isOutofRange( x, y))
+  {
+	SERIAL_ECHOLN(x); SERIAL_ECHOLN(y);
+	return -1;
+  }
+  uint16_t ret = tft_printf(x, y, 0, utf8_str);
+  setPrintPos(x + ret, y);
+  return ret;
+}
+
+int lcd_put_u8str_max_P(PGM_P utf8_str_P, pixel_len_t max_length) {
+uint16_t x = getPrintCol(), y = getPrintRow();
+  if( isOutofRange( x, y))
+  {
+	SERIAL_ECHOLN(x); SERIAL_ECHOLN(y);
+	return -1;
+  }  
+
+  char msg[64];
+  readPGM( utf8_str_P, msg);
+  
+  uint16_t ret = tft_printf(x, y, 0, msg);
+  setPrintPos(x + ret, y);
+  return ret;
+}
+
+#endif // HAS_GRAPHICAL_LCD
---
+++ Marlin/Marlin/src/lcd/dogm/ultralcd_DOGM.h
@@ -0,0 +1,64 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * lcd/dogm/ultralcd_DOGM.h
+ */
+
+#include "../../inc/MarlinConfigPre.h"
+
+
+
+#ifndef LCD_PIXEL_WIDTH
+  #define LCD_PIXEL_WIDTH 320
+#endif
+#ifndef LCD_PIXEL_HEIGHT
+  #define LCD_PIXEL_HEIGHT 240
+#endif
+
+extern int page;
+
+// For selective rendering within a Y range
+#define PAGE_OVER(ya)         ((ya) >= (page*LCD_PIXEL_HEIGHT) ) // Does the current page follow a region top?
+#define PAGE_UNDER(yb)        ((yb) <= (page+1)*LCD_PIXEL_HEIGHT - 1 ) // Does the current page precede a region bottom?
+#define PAGE_CONTAINS(ya, yb) ((yb) <= ((page+1)*LCD_PIXEL_HEIGHT - 1) && (ya) >= ((page*LCD_PIXEL_HEIGHT))) // Do two vertical regions overlap?
+
+
+#define PADDING			  10
+#define MENU_FONT_WIDTH   12
+#define MENU_FONT_ASCENT  20
+#define MENU_FONT_DESCENT 20
+#define MENU_FONT_HEIGHT  (MENU_FONT_ASCENT + MENU_FONT_DESCENT)
+
+#define EDIT_FONT_WIDTH   MENU_FONT_WIDTH
+#define EDIT_FONT_ASCENT  MENU_FONT_ASCENT
+#define EDIT_FONT_DESCENT MENU_FONT_DESCENT
+#define EDIT_FONT_HEIGHT  MENU_FONT_HEIGHT
+
+
+#define INFO_FONT_WIDTH   MENU_FONT_WIDTH
+#define INFO_FONT_ASCENT  MENU_FONT_ASCENT
+#define INFO_FONT_DESCENT MENU_FONT_DESCENT
+#define INFO_FONT_HEIGHT  MENU_FONT_HEIGHT
+
+
